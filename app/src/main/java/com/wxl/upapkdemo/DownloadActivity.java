db6e21d8a56f3ea55910a17ffbd7f113a4f38176
wuxiulin <619663954@qq.com>
2017-12-04 18:43:50 +0800

    使用下载管理器安装

--- app/src/main/java/com/wxl/upapkdemo/DownloadActivity.java
+++ app/src/main/java/com/wxl/upapkdemo/DownloadActivity.java
@@ -10,6 +10,9 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2017/12/2.
@@ -22,14 +25,34 @@ public class DownloadActivity extends AppCompatActivity {
     //"content://downloads/my_downloads"必须这样写不可更改
     public static final Uri CONTENT_URI = Uri.parse("content://downloads/my_downloads");
     private DownloadChangeObserver mObserver;
+    private ProgressBar mProgressBar;
+    private TextView mDownStatus;
+    private TextView mDownDes;
+    private DownloadManager mManager;
+    private DownloadManager.Query mQuery;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download);
+        mDownDes = findViewById(R.id.des);
+        mDownStatus = findViewById(R.id.down_status);
+        mProgressBar = findViewById(R.id.progressBar);
+        mProgressBar.setMax(100);
+
         mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         mId = getIntent().getLongExtra("id",-1);
         Log.e("===","=id="+mId);
+        mQuery = new DownloadManager.Query();
+        mQuery.setFilterById(mId);
+        mManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        mDownStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mManager.remove(mId);
+            }
+        });
 
         mObserver = new DownloadChangeObserver(null);
         getContentResolver().registerContentObserver(CONTENT_URI,true, mObserver);
@@ -44,10 +67,7 @@ public class DownloadActivity extends AppCompatActivity {
 
         @Override
         public void onChange(boolean selfChange) {
-            DownloadManager.Query query = new DownloadManager.Query();
-            query.setFilterById(mId);
-            DownloadManager dManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-            final Cursor cursor = dManager.query(query);
+            final Cursor cursor = mManager.query(mQuery);
             if (cursor != null && cursor.moveToFirst()) {
                 final int totalColumn = cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES);
                 final int currentColumn = cursor.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR);
@@ -55,13 +75,27 @@ public class DownloadActivity extends AppCompatActivity {
                 int currentSize = cursor.getInt(currentColumn);
                 float percent = (float) currentSize / (float) totalSize;
                 int progress = Math.round(percent * 100);
-                Log.e("===","progress="+progress);
+                Log.e("===","progress = "+progress);
+                mProgressBar.setProgress(progress);
+
                 if(progress == 100) {
 
                 }
-            }
-        }
+                //下载文件的URL链接
+                String url =cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI));
 
+                //下载的文件到本地的目录
+                String address = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                mDownDes.setText("正在下载 "+progress+"%");
+
+                Log.e("===","下载文件的URL链接="+url);
+                Log.e("===","address="+address);
+                int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                Log.e("","");
+
+            }
+
+        }
 
     }
 
--- app/src/main/java/com/wxl/upapkdemo/MainActivity.java
+++ app/src/main/java/com/wxl/upapkdemo/MainActivity.java
@@ -1,10 +1,15 @@
 package com.wxl.upapkdemo;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RemoteViews;
 
 import java.util.List;
 
@@ -12,13 +17,17 @@ import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
     private String baseUrl = "http://192.168.120.26:8080/";
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_PRE = 1; //上一首
+    private static final int NOTIFICATION_NEXT = 2; //下一首
+    private static final int NOTIFICATION_OPEN = 3; //打开歌曲
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
 
         //添加OkHttp
@@ -62,6 +71,54 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 //            }
 //        });
 
+    }
+    private void createCustomNotification() {
+        Intent intent = new Intent(this, DownloadActivity.class);
+        //如果第二次获取并且请求码相同,如果原来已解决创建了这个PendingIntent,则复用这个类,并更新intent
+        int flag = PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 3, intent, flag);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setTicker("当前正在播放..")
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("歌曲名")
+                .setContentText("歌手") //以上的设置是在为了兼容3.0之前的版本
+                .setContentIntent(contentIntent)
+                .setContent(getRemoteView()); //自定义通知栏view的api是在3.0之后生效
+        Notification notification = builder.build();
+        //打开通知
+        mNotificationManager.notify(111, notification);
+    }
+    /**
+     * 创建RemoteViews,3.0之后版本使用
+     *
+     * @return
+     */
+    public RemoteViews getRemoteView() {
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.custom_notification);
+        remoteViews.setTextViewText(R.id.tv_content_title, "歌曲名");
+        remoteViews.setTextViewText(R.id.tv_content_text, "歌手");
+        //打开上一首
+        remoteViews.setOnClickPendingIntent(R.id.btn_pre, getClickPendingIntent(NOTIFICATION_PRE));
+        //打开下一首
+        remoteViews.setOnClickPendingIntent(R.id.btn_next, getClickPendingIntent(NOTIFICATION_NEXT));
+        //点击整体布局时,打开播放器
+        remoteViews.setOnClickPendingIntent(R.id.ll_root, getClickPendingIntent(NOTIFICATION_OPEN));
+        return remoteViews;
+    }
+
+    /**
+     * 获取点击自定义通知栏上面的按钮或者布局时的延迟意图
+     *
+     * @param what 要执行的指令
+     * @return
+     */
+    public PendingIntent getClickPendingIntent(int what) {
+        Intent intent = new Intent(this, DownloadActivity.class);
+        intent.putExtra("cmd", what);
+        int flag = PendingIntent.FLAG_UPDATE_CURRENT;
+        PendingIntent clickIntent = PendingIntent.getActivity(this, what, intent, flag );
+        return clickIntent;
     }
 
 
